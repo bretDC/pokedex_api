0dc536b82fccaeb0e3382349b76c68a7d81eb210
Bret <73080023+bretThinkful@users.noreply.github.com>
2021-01-01 10:37:38 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
+
+const app = express()
+
+app.use(morgan('dev'))
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+  
+    // move to the next middleware
+    next()
+})
+
+function handleGetTypes(req, res) {
+res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost: ${PORT}`)
+})
